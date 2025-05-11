6886470ae58c360453cb226e0a35f3a389788fbe
Brad Eckert <brad.sunvn@gmail.com>
2025-05-10 21:39:12 -0700

    Update randkey.c

--- src/tests/randkey.c
+++ src/tests/randkey.c
@@ -7,9 +7,7 @@
 #include "../blake2s/src/blake2s.h"
 #include "../mole.h"
 
-//    ctx->hInitFn((void *)&*ctx->rhCtx, &key[32], 16, MOLE_KEY_HASH_KEY);
-//    for (int i=0; i < 48; i++) ctx->hputcFn((void *)&*ctx->rhCtx, key[i]);
-//    ctx->hFinalFn((void *)&*ctx->rhCtx, ctx->hmac);
+#define MaximumKeys 32
 
 static uint8_t getc_RNG(void) {
     rand();
@@ -17,32 +15,36 @@ static uint8_t getc_RNG(void) {
 }
 
 blake2s_state hCtx; // HMAC context
+char *keyname[MaximumKeys];
 
 int main(int argc, char *argv[]) {
-    char *keyname = "MY_KEY";
-    char *filename = NULL;
+    keyname[0] = "MY_KEY";
+    int keys = 1;
     if (argc > 1) {
-        keyname = argv[1];
+        keys = argc - 1;
+        if (keys >= MaximumKeys) {
+            printf("Too many keys ");
+            return 1;
+        }
+        for (int i = 0; i < keys; i++) {
+            keyname[i] = argv[i + 1];
+        }
     }
-    if (argc > 2) {
-        filename = argv[2];
-    }
-    FILE * file = fopen(filename, "w");
-    if (file == NULL) file = stdout;
     srand(time(0)); // seed with time
-    uint8_t k[MOLE_KEYSET_LENGTH];
-    for (int i=0; i < MOLE_KEYSET_LENGTH; i++) k[i] = getc_RNG();
-    b2s_hmac_init(&hCtx, &k[32], 16, MOLE_KEY_HASH_KEY);
-    for (int i=0; i < (MOLE_KEYSET_HMAC); i++) {
-        b2s_hmac_putc(&hCtx, k[i]);
+    for (int i = 0; i < keys; i++) {
+        uint8_t k[MOLE_KEYSET_LENGTH];
+        for (int i=0; i < MOLE_KEYSET_LENGTH; i++) k[i] = getc_RNG();
+        b2s_hmac_init(&hCtx, &k[32], 16, MOLE_KEY_HASH_KEY);
+        for (int i=0; i < (MOLE_KEYSET_HMAC); i++) {
+            b2s_hmac_putc(&hCtx, k[i]);
+        }
+        b2s_hmac_final(&hCtx, &k[MOLE_KEYSET_HMAC]);
+        printf("#define %s { ", keyname[i]);
+        for (uint8_t i = 0; i < MOLE_KEYSET_LENGTH; i++) {
+            if ((i % 16) == 0) printf("\\ \n  ");
+            printf("0x%02X", k[i]);
+            if (i != (MOLE_KEYSET_LENGTH-1)) printf(", ");
+        }
+        printf("}\n\n");
     }
-    b2s_hmac_final(&hCtx, &k[MOLE_KEYSET_HMAC]);
-    fprintf(file, "#define %s { ", keyname);
-    for (uint8_t i = 0; i < MOLE_KEYSET_LENGTH; i++) {
-        if ((i % 16) == 0) fprintf(file, "\\ \n  ");
-        fprintf(file, "0x%02X", k[i]);
-        if (i != (MOLE_KEYSET_LENGTH-1)) fprintf(file, ", ");
-    }
-    fprintf(file, "};\n");
-    fclose(file);
 }
