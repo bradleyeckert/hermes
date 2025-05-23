4630213f1d2805c7530cbdad039adca1e73d1181
Brad Eckert <brad.sunvn@gmail.com>
2025-05-23 15:32:24 -0700

    Update molesync.bat

--- src/molesync.bat
+++ src/molesync.bat
@@ -1,4 +1,13 @@
 @echo off
+echo Only run this batch file if you want to overwrite files
+echo with the latest from the local blake2s and xchacha repos.
+echo.
+set /P yesno=Do you want to continue? (y/n) 
+if /i "%yesno%"=="y" goto continue
+echo Exiting...
+exit
+:continue
+
 if exist ..\..\blake2s (
   copy ..\..\blake2s\src\blake2s.c blake2s.c /y
   copy ..\..\blake2s\src\blake2s.h blake2s.h /y
